6fbf2201ada5cf490d5d781a211182571393cf75
Sơn ĐInh Trung <SON.DT13376@sinhvien.hoasen.edu.vn>
2025-08-17 15:26:56 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Card from './screens/Card';
+import ConfirmCheckOut from './screens/ConfirmCheckOut';
+import SuccessCheckOut from './screens/SuccessCheckOut';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Card" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Card" component={Card} options={{ title: 'Card'}}/>
+        <Stack.Screen name="ConfirmCheckOut" component={ConfirmCheckOut} options={{ title: 'ConfirmCheckOut'}}/>
+        <Stack.Screen name="SuccessCheckOut" component={SuccessCheckOut} options={{ title: 'SuccessCheckOut'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
